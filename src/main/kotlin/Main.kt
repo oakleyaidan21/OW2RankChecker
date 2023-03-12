e4795bda3fd5b87249b71dd596a6b5e494b0ef2f
oakleyaidan21 <36863200+oakleyaidan21@users.noreply.github.com>
2023-03-12 17:45:20 -0400

    add demo code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,34 @@
-fun main(args: Array<String>) {
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
 
+suspend fun main() {
+    val token = System.getenv("BOT_TOKEN")
+
+    if(token == null) {
+        println("Bot token not set")
+        return
+    }
+
+    val kord = Kord(token)
+
+    kord.on<MessageCreateEvent> { // runs every time a message is created that our bot can read
+        println("Message received! ${message.content}")
+        // ignore other bots, even ourselves. We only serve humans here!
+        if (message.author?.isBot != false) return@on
+
+        // check if our command is being invoked
+        if (message.content != "!ping") return@on
+
+        // all clear, give them the pong!
+        message.channel.createMessage("pong!")
+    }
+
+    kord.login {
+        // we need to specify this to receive the content of messages
+        @OptIn(PrivilegedIntent::class)
+        intents += Intent.MessageContent
+    }
 }
